bf9b6701173cdfae02fd652518aaeba4f06bcc6c
MeiryDiniz <>
2023-10-16 00:18:58 +0000

    Correct file name

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file('cred.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
